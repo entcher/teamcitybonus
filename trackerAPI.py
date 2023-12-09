5f39c36eac5e1d10e16d4d5b7315000387a9e897
entcher <49116032+entcher@users.noreply.github.com>
2023-12-09 12:21:43 +0300

    Add Tracker API

--- trackerAPI.py
+++ trackerAPI.py
@@ -1,3 +1,18 @@
 import requests
 
-print('~~~~~~~~~~~~~~100~~~~~~~~~~~~~~~~~~~~~')
+url = 'https://api.tracker.yandex.net/v2/issues'
+
+headers = {
+    'Authorization': 'OAuth y0_AgAAAAANtcFqAAr2aAAAAAD0PvXsTnXbRKw-RzeSGuyMVve_CcFVaZY',
+    'X-Cloud-Org-ID': 'bpfvb38s0s3vl8njr3n6',
+    'Content-Type': 'application/json'
+}
+
+data = {
+    'queue': 'TEAMCITYBUILDFA',
+    'summary': 'Fix failed build',
+    'assignee': 'captainrall'
+}
+
+response = requests.post(url, headers=headers, json=data)
+print(response)
